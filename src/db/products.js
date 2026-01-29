be9618e8f7eca6accdc04705f67d7ff356ad83ed
bnursik <nbeusenbek@mail.ru>
2026-01-29 18:23:01 +0500

    small bug fix

--- src/db/products.js
+++ src/db/products.js
@@ -1,8 +1,8 @@
-const { getDb } = require("../db");
+const { getDB } = require("../db/client");
 const { COLLECTION_NAME } = require("../config/constants");
 
 function getProductsCollection() {
-  const db = getDb();
+  const db = getDB();
   return db.collection(COLLECTION_NAME);
 }
 
